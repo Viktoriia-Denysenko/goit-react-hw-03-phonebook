24106ad5f819c28d4f98bfc7ad33966ce23f8a4c
Viktoriia Denysenko <denysenko.v.t@gmail.com>
2022-06-12 14:38:37 +0300

    done
    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,22 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+          this.setState({contacts: parsedContacts})
+    }
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
+
   addContact = ({name, number}) => {
     const contact = {
       id: nanoid(),
